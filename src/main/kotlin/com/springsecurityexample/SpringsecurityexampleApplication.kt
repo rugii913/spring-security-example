ac578deb0d91d821ef420cb5ba56f02027b04dce
rugii913 <rugii913@gmail.com>
2024-05-30 21:08:55 +0900

    chore: 프로젝트 시작 설정 및 동작 확인
    프로젝트 시작 설정 및 동작 확인
    - 종속성: Spring Web

---
+++ src/main/kotlin/com/springsecurityexample/SpringsecurityexampleApplication.kt
@@ -0,0 +1,11 @@
+package com.springsecurityexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringsecurityexampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringsecurityexampleApplication>(*args)
+}
