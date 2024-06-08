d0ca814a6d8e641ee98e4f72ec58b7d6623e2ce8
rugii913 <rugii913@gmail.com>
2024-06-08 22:18:34 +0900

    fix: 요구사항에 따라 공개된 엔드포인트 설정 및 작동 확인
    요구사항에 따라 공개된 엔드포인트 설정 및 작동 확인
    - requestMatchers(..).permitAll() 사용

--- src/main/kotlin/com/springsecurityexample/config/ProjectSecurityConfig.kt
+++ src/main/kotlin/com/springsecurityexample/config/ProjectSecurityConfig.kt
@@ -24,16 +24,37 @@ class ProjectSecurityConfig {
     *   - (참고) https://docs.spring.io/spring-security/reference/migration-7/configuration.html
     *     - Spring Security 7부터 기존 방식 완전 제거
     * */
+    /*
+    * - http.authorizeHttpRequests() 살펴보기
+    *   - 위 메서드의 parameter type은 org.springframework.security.config 패키지의
+    *     - Customizer<org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer.AuthorizationManagerRequestMatcherRegistry>
+    *   - org.springframework.security.config 패키지의 Functional Interface인 Customizer는
+    *     - T를 파라미터로 받고 return type은 void인 abstract 메서드 customize()를 갖고 있음
+    *       - cf. 파라미터 T를 받고 아무 일도 하지 않는 static method withDefaults()도 갖고 있음 → default 그대로 이용하고 싶을 때 사용
+    *   - HttpSecurity의 authorizeHttpRequests() 메서드의 본문은 간단함
+    *     - ApplicationContext context = this.getContext();
+    *     - authorizeHttpRequestsCustomizer.customize(((AuthorizeHttpRequestsConfigurer)this.getOrApply(new AuthorizeHttpRequestsConfigurer(context))).getRegistry());
+    *     - return this;
+    *   - 사용자가 구현한 Customizer를 받아서 customize()를 호출
+    *     - 그런데 위에서 확인했듯이 generic type으로 customize()가 받는 parameter type을 AuthorizationManagerRequestMatcherRegistry로 제한해두었음
+    *     - 이에 따라 Customizer를 구현할 때 AuthorizationManagerRequestMatcherRegistry type이 갖고 있는
+    *     - requestMatchers() 등의 메서드를 활용할 수 있음
+    *   - 따라서 실제로 customize()를 구현할 때 조작하는 인자는
+    *     - AuthorizationManagerRequestMatcherRegistry type의 변수이지만
+    *     - 개발자가 생각하기에는 requests로 추상화시켜서 사용한다고 생각할 수 있음
+    * */
 
     @Bean
     @Order(SecurityProperties.BASIC_AUTH_ORDER)
     fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        /* SecurityFilterChainConfiguration의 defaultSecurityFilterChain() 메서드 그대로 가져온 형태 */
-        http.authorizeHttpRequests { requests ->
-            requests.anyRequest().authenticated()
-        }
-        http.formLogin(Customizer.withDefaults())
-        http.httpBasic(Customizer.withDefaults())
-        return http.build()
+        return http
+            .authorizeHttpRequests { requests ->
+                requests
+                    .requestMatchers("/my-account", "my-balance", "/my-loans", "/my-cards").authenticated()
+                    .requestMatchers("/notices", "/contact").permitAll()
+            }
+            .formLogin(Customizer.withDefaults())
+            .httpBasic(Customizer.withDefaults())
+            .build()
     }
 }
