da57d349d533ed05332d12042bf41021be06759a
rugii913 <rugii913@gmail.com>
2024-06-14 10:49:48 +0900

    fix: 프론트엔드에서 호출하는 백엔드 API url 수정
    프론트엔드에서 호출하는 백엔드 API url 수정
    - 백엔드 엔드포인트를 예제와 다르게 변경한 부분에 맞춰 프론트엔드에서 호출하는 url도 수정

--- frontend/src/app/constants/app.constants.ts
+++ frontend/src/app/constants/app.constants.ts
@@ -2,9 +2,9 @@
 export const  AppConstants = {
     CONTACT_API_URL : "/contact",
     LOGIN_API_URL : "/user",
-    ACCOUNT_API_URL : "/myAccount",
-    BALANCE_API_URL : "/myBalance",
-    LOANS_API_URL : "/myLoans",
-    CARDS_API_URL : "/myCards",
+    ACCOUNT_API_URL : "/my-account",
+    BALANCE_API_URL : "/my-balance",
+    LOANS_API_URL : "/my-loans",
+    CARDS_API_URL : "/my-cards",
     NOTICES_API_URL : "/notices"
 }
