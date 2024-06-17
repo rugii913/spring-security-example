df3961ae9ae1025dd7411c71581daa8a6df14771
rugii913 <rugii913@gmail.com>
2024-06-17 18:42:29 +0900

    add: authority 대신 role 기반으로 접근을 제어하기 위해 record 수정
    authority 대신 role 기반으로 접근을 제어하기 위해 record 수정
    - authorities 테이블에서 authority는 모두 삭제
    - role 데이터 삽입

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -255,7 +255,11 @@ CREATE TABLE `authorities` (
     CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`)
 );
 
-INSERT INTO `authorities` (`customer_id`, `name`) VALUES (1, 'VIEWACCOUNT');
-INSERT INTO `authorities` (`customer_id`, `name`) VALUES (1, 'VIEWCARDS');
-INSERT INTO `authorities` (`customer_id`, `name`) VALUES (1, 'VIEWLOANS');
-INSERT INTO `authorities` (`customer_id`, `name`) VALUES (1, 'VIEWBALANCE');
+-- INSERT INTO `authorities` (`customer_id`, `name`) VALUES (1, 'VIEWACCOUNT');
+-- INSERT INTO `authorities` (`customer_id`, `name`) VALUES (1, 'VIEWCARDS');
+-- INSERT INTO `authorities` (`customer_id`, `name`) VALUES (1, 'VIEWLOANS');
+-- INSERT INTO `authorities` (`customer_id`, `name`) VALUES (1, 'VIEWBALANCE');
+-- DELETE FROM `authorities`;
+
+INSERT INTO `authorities` (`customer_id`, `name`) VALUES (1, 'ROLE_USER');
+INSERT INTO `authorities` (`customer_id`, `name`) VALUES (1, 'ROLE_ADMIN');
