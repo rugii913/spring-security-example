b3ebb66c0a87e0a9d6089a774d69bbb5b657ef76
rugii913 <rugii913@gmail.com>
2024-07-08 21:22:24 +0900

    chore: JWT 관련 종속성 추가
    JWT 관련 종속성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,11 @@ dependencies {
     /* Spring Security */
     implementation("org.springframework.boot:spring-boot-starter-security")
 
+    /* JWT 관련 */
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
     /* ldap 관련 */
     implementation("org.springframework.ldap:spring-ldap-core")
     implementation("org.springframework.security:spring-security-ldap")
