b5407aa3451d56f7446ef0e41a46cac2e4b7ac93
rugii913 <rugii913@gmail.com>
2024-07-15 23:49:39 +0900

    chore: 디버깅을 위해 작성된 부분 주석 처리
    디버깅을 위해 작성된 부분 주석 처리
    - @EnableWebSecurity(debug = true) 주석 처리
    - FilterChainProxy의 log level debug 설정 주석 처리

--- src/main/kotlin/com/springsecurityexample/SpringsecurityexampleApplication.kt
+++ src/main/kotlin/com/springsecurityexample/SpringsecurityexampleApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 // - Spring Boot를 사용한다면 SpringSecurity를 사용하더라도 @EnableWebSecurity를 굳이 명시할 필요 없음
-@EnableWebSecurity(debug = true) // debug = true 옵션을 주기 위해 명시, 기본값 false // **debug 옵션은 운영 환경에서는 사용 x**
+// @EnableWebSecurity(debug = true) // debug = true 옵션을 주기 위해 명시, 기본값 false // **debug 옵션은 운영 환경에서는 사용 x**
 // - 패키지가 적절하다면 아래는 명시할 필요 없음
 // @EnableJpaRepositories(basePackages = ["com.springsecurityexample.repository"])
 // @EntityScan(basePackages = ["com.springsecurityexample.model"])
