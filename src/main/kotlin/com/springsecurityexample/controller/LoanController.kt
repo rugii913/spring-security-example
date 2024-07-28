c309e7cb8826d79bb2a3fdecd6786cf17f48c97a
rugii913 <rugii913@gmail.com>
2024-07-28 15:42:43 +0900

    feat: @PostAuthorize 사용 method level security 예시
    @PostAuthorize 사용 method level security 예시

--- src/main/kotlin/com/springsecurityexample/controller/LoanController.kt
+++ src/main/kotlin/com/springsecurityexample/controller/LoanController.kt
@@ -2,6 +2,7 @@ package com.springsecurityexample.controller
 
 import com.springsecurityexample.model.Loan
 import com.springsecurityexample.repository.LoanRepository
+import org.springframework.security.access.prepost.PostAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -11,6 +12,10 @@ class LoanController(
     private val loanRepository: LoanRepository,
 ) {
 
+    /* controller layer의 method에 method level security를 적용해도 layer 상관 없이 동작함을 확인할 수 있음 */
+    /* @PreAuthorize와는 달리 method가 호출은 되지만 return 후  */
+    // @PostAuthorize("hasRole(\"ROOT\")") // 존재하지 않는 role에만 인가하여 method level security 동작 확인
+    @PostAuthorize("hasRole(\"USER\")") // 존재하는 role로 인가하여 method level security 동작 확인
     @GetMapping("/my-loans")
     fun getLoanDetails(@RequestParam(name = "id") customerId: Int): List<Loan> {
         return loanRepository.findByCustomerIdOrderByStartDateDesc(customerId)
--- src/main/kotlin/com/springsecurityexample/repository/LoanRepository.kt
+++ src/main/kotlin/com/springsecurityexample/repository/LoanRepository.kt
@@ -10,6 +10,6 @@ interface LoanRepository : CrudRepository<Loan, Long> {
 
     /* layer 상관 없이 동작함을 확인하기 위해 repository layer의 method에 method level security 적용 */
     // @PreAuthorize("hasRole('ROOT')") // 존재하지 않는 role에만 인가하여 method level security 동작 확인
-    @PreAuthorize("hasRole('USER')") // 존재하는 role로 인가하여 method level security 동작 확인
+    // @PreAuthorize("hasRole('USER')") // 존재하는 role로 인가하여 method level security 동작 확인
     fun findByCustomerIdOrderByStartDateDesc(customerId: Int): List<Loan>
 }
